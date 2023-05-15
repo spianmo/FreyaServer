381d67d547d066a7d0c4e5be599457fc51a43961
Finger <Finger@spianmo.com>
2023-05-16 02:54:08 +0800

    feat: use redis to record sessionKey

--- MainModule/src/main/java/com/kirshi/freya/server/http/RemoteController.java
+++ MainModule/src/main/java/com/kirshi/freya/server/http/RemoteController.java
@@ -1,13 +1,20 @@
 package com.kirshi.freya.server.http;
 
 import com.kirshi.freya.server.base.BaseResponse;
-import com.kirshi.freya.server.base.HttpStatus;
 import com.kirshi.freya.server.service.RemoteService;
-import com.kirshi.freya.server.utils.RandomUtil;
-import io.swagger.annotations.Authorization;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+
+/**
+ * Copyright (c) 2023
+ * @Project:FreyaServer
+ * @Author:Finger
+ * @FileName:RemoteController.java
+ * @LastModified:2023-05-16T02:54:07.583+08:00
+ */
 
 @RestController
 @RequestMapping("/remote")
